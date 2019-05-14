4310d70ccc59bd8533de17d87e9653ef6c225057
Dylan Attal <attal.dylan@gmail.com>
2019-05-14 14:55:12 -0400

    Creates PartSelector component

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,6 @@
       </nav>
     </header>
     <main>
-      <!-- <HomePage/> -->
       <RobotBuilder/>
     </main>
   </div>
@@ -30,15 +29,16 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-}
-
 body {
   background: linear-gradient(to bottom, #555, #999);
   background-attachment: fixed;
 }
+</style>
 
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
 main {
   margin: 0 auto;
   padding: 30px;
@@ -46,7 +46,6 @@ main {
   width: 1024px;
   min-height: 300px;
 }
-
 header {
   background-color: #999;
   width: 1084px;
