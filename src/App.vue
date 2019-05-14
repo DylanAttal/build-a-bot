ce01545c0a9837ce7105f4e45c9d68bdfde56d1b
Dylan Attal <attal.dylan@gmail.com>
2019-05-14 12:48:31 -0400

    HomePage created. Header and nav created.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png" alt="Head of a robot.">
+            Build a Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
     <main>
-      <img alt="Vue logo" src="./assets/logo.png">
       <HomePage/>
     </main>
   </div>
@@ -21,10 +30,38 @@ export default {
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: #fff;
+  width: 1024px;
+  min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
