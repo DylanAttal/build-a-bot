0e32f542b4730900246675ce5e973a2074dcb289
Dylan Attal <attal.dylan@gmail.com>
2019-05-16 10:09:32 -0400

    Can pass styling data directly to v-pin on PartSelector

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,7 +1,11 @@
 export default {
-  bind: element => {
+  bind: (element, binding) => {
+    // Allows us to pass in different pixel values for each offset position
+    // in an object directly attached to v-pin on PartSelector
+    // ex: { top: '5px', left: '10px' }
+    Object.keys(binding.value).forEach(position => {
+      element.style[position] = binding.value[position]
+    })
     element.style.position = 'absolute'
-    element.style.bottom = '5px'
-    element.style.right = '5px'
   }
 }
