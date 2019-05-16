2e6df1bfb3e8cd2e8be848634b75644aee6e35a0
Dylan Attal <attal.dylan@gmail.com>
2019-05-16 09:37:16 -0400

    Uses mapActions helper instead of dispatch

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -53,6 +53,7 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 import createdHookMixin from "./created-hook-mixin";
 import PartSelector from "./PartSelector.vue";
 import CollapsibleSection from "../shared/CollapsibleSection.vue";
@@ -60,7 +61,7 @@ import CollapsibleSection from "../shared/CollapsibleSection.vue";
 export default {
   name: "RobotBuilder",
   created() {
-    this.$store.dispatch("getParts");
+    this.getParts();
   },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
@@ -103,6 +104,7 @@ export default {
     }
   },
   methods: {
+    ...mapActions("robots", ["getParts", "addRobotToCart"]),
     addToCart() {
       const robot = this.selectedRobot;
       const cost =
@@ -111,9 +113,9 @@ export default {
         robot.torso.cost +
         robot.rightArm.cost +
         robot.base.cost;
-      this.$store
-        .dispatch("addRobotToCart", Object.assign({}, robot, { cost }))
-        .then(() => this.$router.push("/cart"));
+      this.addRobotToCart(Object.assign({}, robot, { cost })).then(() =>
+        this.$router.push("/cart")
+      );
       this.addedToCart = true;
     }
   }
--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -2,20 +2,28 @@ import axios from 'axios'
 
 export default {
   state: {
-    user: null
+    user: null,
+    foo: 'users-foo'
   },
   mutations: {
     updateCurrentUser(state, user) {
       state.user = user
     }
   },
-  getters: {},
+  getters: {
+    foo(state) {
+      return `users-getter/${state.foo}`
+    }
+  },
   actions: {
     signIn({ commit }) {
       axios
         .post('/api/sign-in')
         .then(result => commit('updateCurrentUser', result.data))
         .catch(console.error)
+    },
+    addRobotToCart() {
+      console.log('users added robot to cart')
     }
   }
 }
